c77259817a567ad7a51a56821f4ad9500d113bc5
Dmitry Dziadevich <dziadevich@gmail.com>
2020-11-12 14:14:43 -0500

    forms

--- tdf/src/app/app.component.ts
+++ tdf/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {User} from "./user";
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  topics = ['Angular', 'React', 'Vue']
+  topics = ['Angular', 'React', 'Vue'];
+  userModel = new User('Rob', 'rob@test.com', 5556667777, '',
+    'morning', true);
 }
 
