41eb6c49c8541f1957561657bd6bd5dde1236f0e
Dmitry Dziadevich <dziadevich@gmail.com>
2020-11-14 17:49:00 -0500

    forms

--- reactive-forms/src/app/app.component.ts
+++ reactive-forms/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormBuilder} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,16 +7,30 @@ import {FormControl, FormGroup} from "@angular/forms";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  registrationForm = new FormGroup({
-    userName: new FormControl('Dmitry'),
-    password: new FormControl(''),
-    confirmPassword: new FormControl(''),
-    address: new FormGroup({
-      city: new FormControl(''),
-      state: new FormControl(''),
-      postalCode: new FormControl(''),
+  constructor(private fb: FormBuilder) {
+  }
+
+  registrationForm = this.fb.group({
+    userName: ['Dmitry'],
+    password: [''],
+    confirmPassword: [''],
+    address: this.fb.group({
+      city: [''],
+      state: [''],
+      postalCode: ['']
     })
   });
+
+  // registrationForm = new FormGroup({
+  //   userName: new FormControl('Dmitry'),
+  //   password: new FormControl(''),
+  //   confirmPassword: new FormControl(''),
+  //   address: new FormGroup({
+  //     city: new FormControl(''),
+  //     state: new FormControl(''),
+  //     postalCode: new FormControl(''),
+  //   })
+  // });
 
   loadAPI() {
     this.registrationForm.setValue({
@@ -24,9 +38,9 @@ export class AppComponent {
       password: 'pass',
       confirmPassword: 'pass',
       address: {
-        city:'Toronto',
-        state:'ON',
-        postalCode:'M5A 4E9'
+        city: 'Toronto',
+        state: 'ON',
+        postalCode: 'M5A 4E9'
       }
     })
     ;
