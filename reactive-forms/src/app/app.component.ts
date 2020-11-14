24e5f5484c2b46b485e19e16d65e39612535eb73
Dmitry Dziadevich <dziadevich@gmail.com>
2020-11-14 15:57:16 -0500

    forms

---
+++ reactive-forms/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import {Component} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  registrationForm = new FormGroup({
+    userName: new FormControl('Dmitry'),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+    address:new FormGroup({
+      city: new FormControl(''),
+      state: new FormControl(''),
+      postalCode: new FormControl(''),
+    })
+  });
+}
